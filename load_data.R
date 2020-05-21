14c2c70b7ef23d30192ab997d08bc8b175dc655c
ashleyx <ashleythomasxavier@gmail.com>
2020-05-21 10:54:57 +0530

    updating unknown in states_daily and adjusting statewise plot size

--- load_data.R
+++ load_data.R
@@ -8,7 +8,7 @@ sapply( c("magrittr","dplyr","readr","reshape2","skimr","ggplot2","gghighlight",
                 install.packages(x)
             }
             suppressPackageStartupMessages(library(x, character.only = TRUE))
-            
+
             x
         },USE.NAMES = FALSE)
 
@@ -18,21 +18,21 @@ sapply( c("magrittr","dplyr","readr","reshape2","skimr","ggplot2","gghighlight",
 
 # This is the file used for testing numbers  ------------------------------
 data_testing_raw <- read_json("https://api.covid19india.org/data.json",simplifyVector = TRUE)
-data_testing_national <- data_testing_raw$tested  %>% 
+data_testing_national <- data_testing_raw$tested  %>%
                      transmute(total = as.integer(totalsamplestested),
                                date = as.Date(updatetimestamp,"%d/%m/%Y"),
                                new_tested = total - lag(total,order_by = date))
 cat("https://api.covid19india.org/data.json\n")
 
-data_testing_state <- read_csv("https://api.covid19india.org/csv/latest/statewise_tested_numbers_data.csv") %>% 
-    mutate("Updated On" = as.Date(`Updated On`,"%d/%m/%Y")) %>% 
+data_testing_state <- read_csv("https://api.covid19india.org/csv/latest/statewise_tested_numbers_data.csv") %>%
+    mutate("Updated On" = as.Date(`Updated On`,"%d/%m/%Y")) %>%
     filter(`Updated On` > "2020-03-01")
-    
+
 cat("https://api.covid19india.org/csv/latest/statewise_tested_numbers_data.csv\n")
 # This file is used for semi-log plot -------------------------------------
 
-data_india <- data_testing_raw$cases_time_series %>% mutate(date = as.Date(date,"%d %b")) %>% 
-    mutate_if(is.character,as.integer) 
+data_india <- data_testing_raw$cases_time_series %>% mutate(date = as.Date(date,"%d %b")) %>%
+    mutate_if(is.character,as.integer)
 
 data_state <- read_json("https://api.covid19india.org/states_daily.json",simplifyVector = TRUE)$states_daily %>%
     melt(id.vars = c("status","date")) %>%
@@ -43,11 +43,13 @@ data_state <- read_json("https://api.covid19india.org/states_daily.json",simplif
     filter(state != "tt") # removing total tally
 data_state$count[is.na(data_state$count)] <- 0
 
-state_mapping <- data.frame(abbrv = strsplit("an,ap,ar,as,br,ch,ct,dd,dn,dl,ga,gj,hr,hp,jk,jh,ka,kl,la,ld,mp,mh,ml,mn,mz,nl,or,py,pb,rj,sk,tn,tg,tr,up,ut,wb", split = ",")[[1]],
-                            full = strsplit("Andaman and Nicobar Islands,Andhra Pradesh,Arunachal Pradesh,Assam,Bihar,Chandigarh,Chhattisgarh,Daman and Diu,Dadra and Nagar Haveli,Delhi,Goa,Gujarat,Haryana,Himachal Pradesh,Jammu and Kashmir,Jharkhand,Karnataka,Kerala,Lakshadweep,Ladakh,Madhya Pradesh,Maharashtra,Meghalaya,Manipur,Mizoram,Nagaland,Odisha,Puducherry,Punjab,Rajasthan,Sikkim,Tamil Nadu,Telangana,Tripura,Uttar Pradesh,Uttarakhand,West Bengal",split =",")[[1]])
+state_mapping <- data.frame(abbrv = strsplit("an,ap,ar,as,br,ch,ct,dd,dn,dl,ga,gj,hr,hp,jk,jh,ka,kl,la,ld,mp,mh,ml,mn,mz,nl,or,py,pb,rj,sk,tn,tg,tr,up,ut,wb,un", split = ",")[[1]],
+                            full = strsplit("Andaman and Nicobar Islands,Andhra Pradesh,Arunachal Pradesh,Assam,Bihar,Chandigarh,Chhattisgarh,Daman and Diu,Dadra and Nagar Haveli,Delhi,Goa,Gujarat,Haryana,Himachal Pradesh,Jammu and Kashmir,Jharkhand,Karnataka,Kerala,Lakshadweep,Ladakh,Madhya Pradesh,Maharashtra,Meghalaya,Manipur,Mizoram,Nagaland,Odisha,Puducherry,Punjab,Rajasthan,Sikkim,Tamil Nadu,Telangana,Tripura,Uttar Pradesh,Uttarakhand,West Bengal,Unknown",split =",")[[1]])
 
 data_state$state %<>% as.character() %>%
     sapply(function(i) state_mapping$full[state_mapping$abbrv ==i])
+
+data_state %<>% filter(state != "Unknown")
 
 cat("https://api.covid19india.org/states_daily.json\n")
 
@@ -94,7 +96,7 @@ cat("https://api.covid19india.org/raw_data[1-4]json")
 
 
 # # population data ---------------------------------------------------------
-# 
+#
 # population_data <-readHTMLTable(readLines("https://en.wikipedia.org/wiki/List_of_states_and_union_territories_of_India_by_population"))[[2]] %>%
 #     {
 #         population_data <- data.frame(.[-1,])
@@ -102,7 +104,7 @@ cat("https://api.covid19india.org/raw_data[1-4]json")
 #         population_data %>%
 #             transmute(state = `State or union territory` %>% gsub("\\[c\\]","",x = .),
 #                       pop = gsub("[^0-9]","",population_data$`Population(%)`) %>%   as.numeric())
-#         
+#
 #     }
 
 cat("All done")
--- statewise_monitoring.R
+++ statewise_monitoring.R
@@ -6,7 +6,7 @@ sapply( c("magrittr","dplyr","reshape2","ggplot2","jsonlite","EpiEstim","patchwo
             install.packages(x)
           }
           suppressPackageStartupMessages(library(x, character.only = TRUE))
-          
+
           x
         },USE.NAMES = FALSE)
 
@@ -31,24 +31,24 @@ data_state %>% group_by(status,state) %>%
   summarise(n = max(tally)) %>% {
     .$state[.$n > 400]
   } %>% lapply(function(s){
-    
+
     print(s)
     data <- data_state %>% group_by(status,state) %>%
-      mutate(tally = cumsum(count)) %>% 
+      mutate(tally = cumsum(count)) %>%
       filter(status == "Confirmed",
              date > date_start,
-             date < Sys.Date()) %>% 
-      filter(state == s) 
-    
+             date < Sys.Date()) %>%
+      filter(state == s)
+
     #  -------------------------------------------------------------
     #  Plotting -------------------------------------------------------------
     #  -------------------------------------------------------------
-    
+
     K <- (log(data$tally[data$date == (Sys.Date() - 1)]) -
             log(data$tally[data$date == (Sys.Date() - 7)]) ) /
       (7 - 1)
-    
-    
+
+
     p1 <- data %>%  filter(status == "Confirmed") %>%
       ggplot(aes(x= date)) +
       geom_histogram(aes(y = count),stat = "identity")+
@@ -60,11 +60,11 @@ data_state %>% group_by(status,state) %>%
       # geom_line(data = predicted,aes(x = date, y = mean), linetype = 2) +
       theme_bw()+
       theme(axis.title.x = element_blank())
-    
+
     Rt <- data %>%
       dcast(data = . , formula = date ~ status, value.var = "count") %>%
       transmute(dates = date , I = Confirmed) %>%
-      filter(I > 0) %>% 
+      filter(I > 0) %>%
       estimate_R(incid = .,
                  method = "parametric_si",
                  config = make_config(list(mean_si = 5.12,std_si = 3)))
@@ -80,10 +80,10 @@ data_state %>% group_by(status,state) %>%
       ggtitle(label = "Effective Reproduction Number")+
       theme_bw() +
       theme(axis.title.x = element_blank())
-    
+
     p3 <- data_testing_state %>% filter(State == s) %>%
       mutate(new_tests = `Total Tested` - lag(`Total Tested`)) %>%
-      filter(`Updated On` > date_start) %>% 
+      filter(`Updated On` > date_start) %>%
       ggplot(aes(x= `Updated On` , y = new_tests))+
       geom_histogram(stat="identity")+
       scale_x_date(expand = c(0,0)) +
@@ -91,60 +91,60 @@ data_state %>% group_by(status,state) %>%
       theme_bw()+
       theme(axis.title.x = element_blank(),
             axis.title.y = element_blank())
-    
+
     # case fatality ratios ----------------------------------------------------
-    
+
     data_cfr <- data_state %>%
       filter(state == s,
-             date < Sys.Date()) %>% 
-      group_by(status) %>% 
-      mutate(count = cumsum(count)) %>% 
+             date < Sys.Date()) %>%
+      group_by(status) %>%
+      mutate(count = cumsum(count)) %>%
       filter(date > date_start)
-    
+
     p4 <- data_cfr %>%
-      group_by(date) %>% 
-      summarise(cfr = 100*count[status == "Deceased"]/(count[status == "Recovered"] + count[status == "Deceased"])) %>% 
-      na.omit() %>% 
-      ggplot(aes(x = date, y = cfr)) + 
+      group_by(date) %>%
+      summarise(cfr = 100*count[status == "Deceased"]/(count[status == "Recovered"] + count[status == "Deceased"])) %>%
+      na.omit() %>%
+      ggplot(aes(x = date, y = cfr)) +
       geom_line() +
       geom_hline(yintercept = 0.75, linetype = 2)+
       scale_x_date(expand = c(0,0)) +
       labs(title = "Case Fatality Ratio %",
           subtitle = "Dotted line is best true estimate of CFR",
-          caption = data_cfr %>% 
+          caption = data_cfr %>%
             filter(date == max(date)) %>% {
               cfr <- .$count[.$status == "Deceased"]/(.$count[.$status == "Recovered"] + .$count[.$status == "Deceased"])
               cfr_true <- c(0.01,0.005)
-              round(cfr/cfr_true,digits = 1) %>% paste0(.,collapse = " - ") %>% 
+              round(cfr/cfr_true,digits = 1) %>% paste0(.,collapse = " - ") %>%
                 paste0("Estimated undercount of cases: (",.,")x")
             }) +
       theme_bw()+
       theme(axis.title.x = element_blank(),
             axis.title.y = element_blank())
-    
-    p5 <- data_testing_state %>% filter(State == s) %>% 
-      filter(`Updated On` > date_start) %>% 
+
+    p5 <- data_testing_state %>% filter(State == s) %>%
+      filter(`Updated On` > date_start) %>%
       transmute(date = as.Date(`Updated On`),
                 positive = Positive,
-                negative = Negative) %>% na.omit() %>% 
+                negative = Negative) %>% na.omit() %>%
       mutate(new_positive = positive - lag(positive, default = 0),
              new_negative = negative - lag(negative, default = 0),
-             percent_new_positive = 100 * new_positive/(new_positive + new_negative)) %>% 
-      ggplot(aes(x= date , y = percent_new_positive))+ 
+             percent_new_positive = 100 * new_positive/(new_positive + new_negative)) %>%
+      ggplot(aes(x= date , y = percent_new_positive))+
       geom_histogram(stat = "identity")+
       ggtitle("Prcentage positive in new tests ") +
       theme_bw() +
       theme(axis.title.y = element_blank(),
             axis.title.x = element_blank())
-    
-    
+
+
     final_plot <- p1 /((p3/p5)|(p2/p4) + plot_layout(widths = c(1,1))) +
       plot_layout(heights = c(1,2))
     final_plot
     ggsave(paste0("statewise_plot/",Sys.Date(),"/",Sys.Date(),"_",gsub(" ","-",s),".png"),
            plot = final_plot,
-           width = 14 , height = 14)
-    
+           width = 8 , height = 8)
+
   }) %>%  invisible()
 
 
